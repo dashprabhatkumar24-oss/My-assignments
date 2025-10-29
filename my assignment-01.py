507ac5d94263445c56e5f114d1dd8116d89beeef
dashprabhatkumar24-oss <dashprabhatkumar24@gmail.com>
2025-10-29 13:20:19 +0530

    Add files via upload

---
+++ my assignment-01.py
@@ -0,0 +1,14 @@
+a=int(input("enter first number"))
+b=int(input("enter secund number"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("addition:",c)
+print("substraction:",d)
+print("multipication:",e)
+print("division:",f)
+s1=input("Enter the first name")
+s2=input("Enter the second name")
+s3=s1+s2
+print(f"Hello, {s3}! Welcome to the python program.")
